3a06cfc41874484c0e34cb2410d3cdf64aceb937
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2018-12-15 15:01:10 -0800

    Complete adding css

--- src/MakeHeartEyesDancer.js
+++ src/MakeHeartEyesDancer.js
@@ -1,5 +1,7 @@
 var MakeHeartEyesDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="heartEyesDancer"></span>');
+  this.setPosition(top, left);
 };
 
 MakeHeartEyesDancer.prototype = Object.create(MakeDancer.prototype); 
@@ -7,5 +9,6 @@ MakeHeartEyesDancer.prototype.constructor = MakeHeartEyesDancer;
 
 MakeHeartEyesDancer.prototype.step = function() {
   MakeDancer.prototype.step.call(this);
-  this.$node.fadeTo('slow', 1);
+  this.$node.animate({ width: "50%" }, 7000 );
+  this.$node.animate({ width: "-50%" }, 7000 );
 };
--- src/MakeMindBlownDancer.js
+++ src/MakeMindBlownDancer.js
@@ -1,5 +1,7 @@
 var MakeMindBlownDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="mindBlownDancer"></span>');
+  this.setPosition(top, left);
 };
 
 MakeMindBlownDancer.prototype = Object.create(MakeDancer.prototype); 
@@ -7,5 +9,7 @@ MakeMindBlownDancer.prototype.constructor = MakeMindBlownDancer;
 
 MakeMindBlownDancer.prototype.step = function() {
   MakeDancer.prototype.step.call(this);
-  this.$node.fadeToggle();
+  // this.$node.fadeToggle();
+  this.$node.animate({ height: "10%" }, 1000 );
+  this.$node.animate({ height: "-5%" }, 1000 );
 };
